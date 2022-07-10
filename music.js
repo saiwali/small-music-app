0fa6a5ddeaf810a7b2a52509eb65bda3162e97b9
Sai Prasad Wali <84082731+saiwali@users.noreply.github.com>
2022-07-10 14:15:36 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,62 @@
+document.querySelector(".song1").addEventListener("click", function() {
+
+  var audio = new Audio('music/alag aasman.mp3');
+  audio.play();
+
+});
+
+document.querySelector(".song2").addEventListener("click", function() {
+
+  var audio = new Audio('music/chidiya.mp3');
+  audio.play();
+
+});
+document.querySelector(".song3").addEventListener("click", function() {
+
+  var audio = new Audio('music/common.mp3');
+  audio.play();
+
+});
+document.querySelector(".song4").addEventListener("click", function() {
+
+  var audio = new Audio('music/demons.mp3.mp3');
+  audio.play();
+  
+
+});
+document.querySelector(".song5").addEventListener("click", function() {
+
+  var audio = new Audio('music/ex i never had.mp3');
+  audio.play();
+
+});
+document.querySelector(".song6").addEventListener("click", function() {
+
+  var audio = new Audio('music/excuses.mp3');
+  audio.play();
+
+});
+document.querySelector(".song7").addEventListener("click", function() {
+
+  var audio = new Audio('music/kaise hua.mp3');
+  audio.play();
+
+});
+document.querySelector(".song8").addEventListener("click", function() {
+
+  var audio = new Audio('music/kya mujhe pyaar hai.mp3');
+  audio.play();
+
+});
+document.querySelector(".song9").addEventListener("click", function() {
+
+  var audio = new Audio('music/one dance.mp3');
+  audio.play();
+
+});
+document.querySelector(".song10").addEventListener("click", function() {
+
+  var audio = new Audio('music/samajavaragamana.mp3');
+  audio.play();
+
+});
